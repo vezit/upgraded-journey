c036dac1e3ebea617ab67011679538f4e5951f9c
Victor Reipur <70368750+vicre@users.noreply.github.com>
2025-06-05 11:00:56 +0200

    Add vault data clear functionality

--- app-main/pages/vault.tsx
+++ app-main/pages/vault.tsx
@@ -1,6 +1,7 @@
 import UploadZone from '@/components/UploadZone'
 import VaultDiagram from '@/components/VaultDiagram'
 import { parseVault } from '@/lib/parseVault'
+import { clearVault } from '@/lib/storage'
 import { useGraph } from '@/contexts/GraphStore'
 
 export default function Vault(){
@@ -9,6 +10,15 @@ export default function Vault(){
     <div className="p-4 flex flex-col gap-4">
       <UploadZone onLoad={data=>setGraph(parseVault(data))} />
       <VaultDiagram />
+      <button
+        onClick={() => {
+          clearVault()
+          setGraph({ nodes: [], edges: [] })
+        }}
+        className="self-start px-4 py-2 bg-red-500 text-white rounded"
+      >
+        Delete Vault Data
+      </button>
     </div>
   )
 }
